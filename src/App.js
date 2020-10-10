124a74e74e7603e5ab164061f7ecdb2a816a0c90
GerardoBetancourt <56419938+GerardoBetgro@users.noreply.github.com>
2020-10-10 16:57:49 -0500

    Summary

--- src/App.js
+++ src/App.js
@@ -1,46 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function NavBar(propiedades) {
-  return (
-    <p>{propiedades.nombre}</p>
-  );
+class NavBar extends Component {
+  render() {
+    const { nombre } = this.props
+    return <p>{nombre}</p>
+  }
 }
-function Movement(propiedades) {
-  return (
-    <div className="movement-group">
+class Movement extends Component {
+  render() {
+    const { amount, name } = this.props
+    return <div className="movement-group">
       <ul>
         <li className="movement income">
-          <span>{propiedades.name} - ${propiedades.amount}</span>
+          <span>{name} - ${amount}</span>
           <span className="delete-btn">&times;</span>
         </li>
       </ul>
     </div>
-  );
+  }
 }
-function Debt(propiedades) {
-  return (
-    <div>
+class Debt extends Component {
+  render() {
+    const { person, debt, date } = this.props
+    return <div>
       <ol>
         <li className="debt">
           <div>
-            <div>{propiedades.person} - ${propiedades.debt}</div>
-            <small>{propiedades.date}</small>
+            <div>{person} - ${debt}</div>
+            <small>{date}</small>
           </div>
           <button className="secondary-btn">Saldar</button>
         </li>
       </ol>
     </div>
-  );
+  }
 }
-function Saving(propiedades) {
-  return (
-    <div>
+class Saving extends Component {
+  render() {
+    const { totalSaved, target, name } = this.props
+    return <div>
       <ul>
         <li className="saving">
           <div>
-            <h4>{propiedades.name}</h4>
-            <small>Total: <b>${propiedades.totalSaved} / {propiedades.target}</b></small>
+            <h4>{name}</h4>
+            <small>Total: <b>${totalSaved} / {target}</b></small>
           </div>
           <div className="bottom-saving-items">
             <input
@@ -53,7 +57,7 @@ function Saving(propiedades) {
         </li>
       </ul>
     </div>
-  );
+  }
 }
 
 function App() {
@@ -64,8 +68,8 @@ function App() {
           <h1><NavBar nombre="Mi flujo financiero🤑💰" /></h1>
           <div className="right-nav-items">
             <NavBar nombre="Usuario: gerardoguerrero" />
-            <a href="www.facebook.com"><NavBar nombre="Ayuda" /></a>
-            <a href="www.google.com"><NavBar nombre="Salir" /></a>
+            <a href="ayuda"><NavBar nombre="Ayuda" /></a>
+            <a href="salir"><NavBar nombre="Salir" /></a>
           </div>
         </div>
       </nav>
@@ -83,15 +87,15 @@ function App() {
           <section>
             <h2>Ingresos</h2>
             <p>Total de ingresos: $19,500</p>
-            <Movement name="Salario" amount="15,000" />
-            <Movement name="Mesada" amount="1,500" />
-            <Movement name="Regalo de tio" amount="3,000" />
+            <Movement name="Salario" amount={15000} />
+            <Movement name="Mesada" amount={1500} />
+            <Movement name="Regalo de tio" amount={3000} />
             <h2>Gastos</h2>
             <p>Total de gastos: $8,220</p>
-            <Movement name="Uber" amount="460" />
-            <Movement name="Netflix" amount="260" />
-            <Movement name="Renta" amount="7,000" />
-            <Movement name="Internet" amount="500" />
+            <Movement name="Uber" amount={460} />
+            <Movement name="Netflix" amount={260} />
+            <Movement name="Renta" amount={7000} />
+            <Movement name="Internet" amount={500} />
           </section>
 
           <section>
@@ -103,10 +107,10 @@ function App() {
               <button className="secondary-btn">Listo</button>
             </div>
             <h3>A favor</h3>
-            <Debt person="Carlos Fernandez" debt="1,230" date="05 de octubre 2020" />
-            <Debt person="Primo de Alex" debt="22,000" date="05 de octubre 2020" />
+            <Debt person="Carlos Fernandez" debt={1230} date="05 de octubre 2020" />
+            <Debt person="Primo de Alex" debt={22000} date="05 de octubre 2020" />
             <h3>Por pagar</h3>
-            <Debt person="Mama" debt="500" date="05 de octubre 2020" />
+            <Debt person="Mama" debt={500} date="05 de octubre 2020" />
           </section>
 
           <section>
@@ -116,8 +120,8 @@ function App() {
               <input type="number" placeholder="Meta (opcional)" />
               <button className="secondary-btn">Listo</button>
             </div>
-            <Saving name="Fondo para emergencias" totalSaved="17,230"/>
-            <Saving name="MacBook Pro 2018" totalSaved="8,230" target="22,400"/>
+            <Saving name="Fondo para emergencias" totalSaved={17230} />
+            <Saving name="MacBook Pro 2018" totalSaved={8230} target={22400} />
           </section>
         </div>
       </main>
